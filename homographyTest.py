0ccf0651848d53577a3ba9b47191f5b80110daf9
Daniel Stratton <dsstratton@outlook.com>
2026-05-01 21:47:28 +1000

    Borrow some roboflow to improve display

--- homographyTest.py
+++ homographyTest.py
@@ -1,9 +1,11 @@
+import argparse
 import cv2
 import numpy as np
-import argparse
+import supervision as sv
 from detectionadapter import DetectionAdapter
-from ultralytics import YOLO, SAM
 from images import get_person_mask
+from pitch import SoccerPitchConfiguration
+from ultralytics import YOLO, SAM
 
 parser = argparse.ArgumentParser( description="Homography test" )
 parser.add_argument( "-input",  help="Input video file",      type=str, default="test_homography_input.mp4" )
@@ -14,6 +16,7 @@ parser.add_argument( "-segm",   help="Segmentation model",    type=str, default=
 parser.add_argument( "-limit",  help="Frame limit",           type=int, default=50)
 
 args = parser.parse_args()
+cfg = SoccerPitchConfiguration()
 
 print( "Configuring paths" )
 input_video_path = args.input
@@ -22,7 +25,7 @@ output_video_path = args.output
 # Load a Model for detection an prepare it
 print( "Loading model" )
 model = YOLO( args.model, verbose=False )
-segm  = SAM( args.segm )
+#segm  = SAM( args.segm )
 
 # Get Video Information
 print( "Getting video information" )
@@ -42,47 +45,20 @@ frame_count: int = 0
 BALL_CLASS_ID = 32
 PLAYER_CLASS_ID = 0
 
-PITCH_HEIGHT = 270
-PITCH_WIDTH = 480
+PITCH_HEIGHT = 272
+PITCH_WIDTH = 420
 PLAYER_RADIUS = 10
+
+STD_PITCH_LENGTH = int(cfg.length / 100)
+STD_PITCH_WIDTH = int(cfg.width / 100)
 
 print( "Configuring Writer" )
 out = cv2.VideoWriter( output_video_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (new_w, new_h) )
 
-PITCH_TEMPLATE_POINTS = np.array([
-  [   0,     0],    # top-left corner
-  [52.5,     0],    # top-middle
-  [ 105,     0],    # top-right corner
-  [ 105,    68],    # bottom-right corner
-  [52.5,    68],    # bottom-middle
-  [   0,    68],    # bottom-left corner
-  [52.5, 29.43],    # top-centre-circle-edge
-  [52.5, 38.57],    # bottom-centre-circle-edge
-  [47.93,   34],    # left-centre-circle-edge
-  [57.07,   34],    # right-centre-circle-edge
-  [   0,  30.344],  # top-goalpost-edge
-  [   0,  37.656],  # bottom-goalpost-edge
-  # Add more known points if you want higher accuracy
-], dtype=np.float32)
-
 # Helper functions
-def image_to_pitch( u, v, H ):
-  pt = np.array( [ [ u, v, 1.0 ] ], dtype=np.float32 ).T
-  mapped = H @ pt
-  mapped /= mapped[ 2, 0 ]
-  #cv2.perspectiveTransform( points, H )
-  return mapped[ 0, 0 ], mapped[ 1, 0 ]
-
 def positions_to_pitch( positions, H ):
   pts = np.array(positions, dtype=np.float32).reshape( -1, 1, 2 )
   return cv2.perspectiveTransform(pts, H).reshape( -1, 2 )
-
-def bbox_bottom_center_to_pitch( x1, y1, x2, y2, H ):
-  # Calculate the middle of the two x points
-  u = 0.5 * (x1 + x2)
-  # And the bottom y coordinate
-  v = y2
-  return image_to_pitch( u, v, H )
 
 def classify_team( hsv_patch ):
   h = hsv_patch[:,:,0].mean()
@@ -103,27 +79,57 @@ def classify_team( hsv_patch ):
 
   return "unknown"
 
-def draw_empty_pitch( width = PITCH_WIDTH, height = PITCH_HEIGHT ):
+def draw_empty_pitch( width = PITCH_WIDTH, height = PITCH_HEIGHT, 
+                      background_color: sv.Color = sv.Color(34, 139, 34),
+                      line_color: sv.Color = sv.Color.WHITE,
+                      padding: int = 50,
+                      line_thickness: int = 4,
+                      point_radius: int = 8
+) -> np.ndarray:
   
+  scaleW = (width / STD_PITCH_LENGTH)/100
+  scaleL = (height / STD_PITCH_WIDTH)/100
+  
+  #print( f"Dimensions: {width}, {height}" )
+  #print( f"Scaled    : {scaleW}, {scaleL}" )
   # Blank out the image
-  pitch = np.zeros( (height, width, 3), dtype=np.uint8 )
+  pitch = np.ones( (height + 2 * padding, width + 2 * padding, 3), dtype=np.uint8 ) * np.array( background_color.as_bgr(), dtype=np.uint8 )
 
-  # Draw outer rectangle
-  cv2.rectangle( pitch, (5,5), (width-5, height-5), (255,255,255), 5 )
+  for start, end in cfg.edges:
+    point1 = (int(cfg.vertices[start - 1][0] * scaleW) + padding, int(cfg.vertices[start - 1][1] * scaleL) + padding)
+    point2 = (int(cfg.vertices[end   - 1][0] * scaleW) + padding, int(cfg.vertices[end   - 1][1] * scaleL) + padding)
+    cv2.line( img=pitch, pt1=point1, pt2=point2, color=line_color.as_bgr(), thickness=line_thickness )
+    #print( f"Point {point1}, {point2}" )
 
-  # Centre line
-  cv2.line( pitch, (width//2, 5), (width//2, height-5), (255,255,255), 5 )
+  yard10 = int(cfg.centre_circle_radius * scaleW)
+  centre_circle_center = ( int(width // 2 + padding), int(height // 2 + padding) )
+  #print( f"Centre {centre_circle_center} x {cfg.centre_circle_radius * scaleW }")
+  cv2.circle(
+      img=pitch,
+      center=centre_circle_center,
+      radius=yard10,
+      color=line_color.as_bgr(),
+      thickness=line_thickness
+  )
 
-  # Centre circle
-  cv2.circle( pitch, (width//2, height//2), int(9.14*(width/105)), (255,255,255), 5 )
+  scaled_penalty = int(cfg.penalty_spot_distance * scaleL)
+  penalty_spots = [
+      ( int(scaled_penalty + padding),         int(height // 2 + padding) ),
+      ( int(width - scaled_penalty + padding), int(height // 2 + padding) )
+  ]
+  arc_angles = [ (-57, 57), (123, 237) ]
+  for spot, (start,end) in zip(penalty_spots, arc_angles):
+    #print( f"Penalty {spot} x {point_radius * scaleW * 10 }")
+    cv2.circle( img=pitch, center=spot, radius=int(point_radius * scaleW * 10), color=line_color.as_bgr(), thickness=-1 )
+    cv2.ellipse( img=pitch, center=spot, axes=(yard10, yard10), angle=0, startAngle=start, endAngle=end, color=line_color.as_bgr(), thickness=2 )
 
   return pitch
 
 def pitch_to_overlay( X, Y, overlay_w, overlay_h ):
   # Default FIFA size is 105 x 68
   # X: 0–105, Y: 0–68
-  px = int( (X / 105) * overlay_w )
-  py = int( (Y / 68) * overlay_h )
+  px = int( (X / STD_PITCH_LENGTH) * overlay_w )
+  py = int( (Y / STD_PITCH_WIDTH) * overlay_h )
   return px, py
 
 def draw_player_on_pitch( pitch_img, X, Y, team ):
@@ -179,7 +185,7 @@ while True:
   if not ret:
     break
   frame_count += 1
-  if frame_count > frame_limit:
+  if frame_count > frame_limit and frame_limit != -1:
     break
 
   print('.', end='', flush=True)
@@ -209,23 +215,23 @@ while True:
     # Team colour classifier
     x1, y1, x2, y2 = map(int, (x1f, y1f, x2f, y2f))
     
-    person_mask = get_person_mask( new_frame, segm, x1, y1, x2, y2 )
-    if person_mask is None:
-      continue
-    
-    jersey_patch = extract_jersey( new_frame, x1, y1, x2, y2, person_mask )
-    if jersey_patch is None:
-      continue
-    
-    hsv = cv2.cvtColor( jersey_patch, cv2.COLOR_BGR2HSV )
-    team = classify_team( hsv )
-    if team == "referee":
-      continue
+    #person_mask = get_person_mask( new_frame, segm, x1, y1, x2, y2 )
+    #if person_mask is None:
+    #  continue
+    #
+    #jersey_patch = extract_jersey( new_frame, x1, y1, x2, y2, person_mask )
+    #if jersey_patch is None:
+    #  continue
+    #
+    #hsv = cv2.cvtColor( jersey_patch, cv2.COLOR_BGR2HSV )
+    #team = classify_team( hsv )
+    #if team == "referee":
+    #  continue
+    team = "home"
     
     # Homography mapping
     positions.append( [0.5 * (x1 + x2), y2] )
     teams.append( team )
-    #X, Y = bbox_bottom_center_to_pitch( x1, y1, x2, y2, H )
 
   mapped = positions_to_pitch( positions, H )
   for (X, Y), team in zip(mapped, teams):
@@ -234,14 +240,15 @@ while True:
   # draw_player_on_pitch( pitch_img, X, Y, team )
   
   # Make sure we print the (hopefully just 1) ball
-  for det in ball_dets:
-    x1, y1, x2, y2, conf, cid = det
-    
-    # Homography mapping
-    X, Y = bbox_bottom_center_to_pitch( x1, y1, x2, y2, H )
-
-    # Draw on mini-pitch
-    draw_player_on_pitch( pitch_img, X, Y, "ball" )
+  
+  #for det in ball_dets:
+  #  x1, y1, x2, y2, conf, cid = det
+  #  
+  #  # Homography mapping
+  #  X, Y = bbox_bottom_center_to_pitch( x1, y1, x2, y2, H )
+#
+  #  # Draw on mini-pitch
+  #  draw_player_on_pitch( pitch_img, X, Y, "ball" )
   
   # Copy our frame
   annotated_frame = new_frame.copy()
